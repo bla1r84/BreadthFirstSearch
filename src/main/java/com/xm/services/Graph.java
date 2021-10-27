1668ca4c6d07881d203de7894619e2bc60625fc4
Ioannis Lilimpakis <blar@hotmail.com>
2021-10-27 12:11:28 +0300

    Refactor - Remove unused import.

--- src/main/java/com/xm/services/Graph.java
+++ src/main/java/com/xm/services/Graph.java
@@ -7,7 +7,6 @@ import com.xm.model.Node;
 import com.xm.model.data.Data;
 import com.xm.utils.PathTraverser;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
